bd5c1dd65ff677911f5c15b5b92b1282fb4874e5
esir-indv <847045180@qq.com>
2023-10-10 19:40:47 +0800

    first commit

---
+++ src/main/java/com/scrcu/AliyunecsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.scrcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunecsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunecsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scrcu/aliyunecsjava/AliyunecsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrcu.aliyunecsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunecsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
