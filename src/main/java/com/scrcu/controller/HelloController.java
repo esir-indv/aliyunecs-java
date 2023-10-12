653f54785ca7dcc5d4e2fe53908ff07ea463f3e9
esir-indv <847045180@qq.com>
2023-10-12 11:20:38 +0800

    postman-@PostMpping请求-user类成功

--- src/main/java/com/scrcu/controller/HelloController.java
+++ src/main/java/com/scrcu/controller/HelloController.java
@@ -1,12 +1,21 @@
 package com.scrcu.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.scrcu.pojo.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class HelloController {
-    @RequestMapping(value = "/hello")
-    public String index(){
-        return "Hello World Fengliangliang";
+    @GetMapping(value = "/hello")
+    public String hello(String name) {
+        return "Hello World :" + name;
     }
+    //请求体封装为一个user类
+    @PostMapping(value = "/user")
+    public String user(@RequestBody User user){
+        log.info("user:" + user);
+        return "POST请求:"+ user.toString();
+    }
+
 }
---
+++ src/main/java/com/scrcu/pojo/User.java
@@ -0,0 +1,22 @@
+package com.scrcu.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: fengliang
+ * @description: TODO
+ * @date: 2023/10/12 09:14
+ * @version: 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String password;
+
+
+}
+
